e7bbd0104cf4ac92b50b39e4bf4afbbce308d8ac
Ravy <ravy@farfrom.earth>
2020-07-17 23:56:27 +0200

    restructure to use fancy files

--- routers/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-// eslint-disable-line new-cap
-const api = express.Router();
-
-api.get('/', (req, res) => {
-	res.status(201).json({ msg: 'Hello World!' });
-});
-
-module.exports = api;
