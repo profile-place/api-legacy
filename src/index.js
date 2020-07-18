a17d4bba30fe1de23616d785f5666b8869635488
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-07-18 11:45:09 -0400

    fix 404 handle

--- src/index.js
+++ src/index.js
@@ -10,13 +10,6 @@ app.use(express.json());
 const { readdir } = require('fs').promises;
 const { join } = require('path').posix;
 
-// Handle errors (not found, whatever)
-app.use((req, res) => {
-	res.status(404).json({
-		message: "Not found"
-	});
-});
-
 readdir(join(__dirname, 'routers')).then(async versions => {
 	for (const version of versions) {
 		const router = require(`./routers/${version}`);
@@ -26,4 +19,16 @@ readdir(join(__dirname, 'routers')).then(async versions => {
 		logger.log(`API listening on port ${port}`);
 		logger.log(`Versions loaded: ${versions.join(', ')}`);
 	});
+
+	// Handle errors (not found, whatever)
+	app.use((req, res) => {
+		res.status(404).json({ msg: 'Not found', info: 'Couldn\'t find that page. Try again later.' });
+	});
+
+	// u guys decide what u want to do with the error (say log only if a certain message code whatever the fuck)
+	/* eslint-disable-next-line handle-callback-err */
+	app.use((err, req, res) => {
+		res.status(500).json({ msg: 'Something went wrong.', info: 'An error occurred. Try again later.' });
+	});
 });
+
