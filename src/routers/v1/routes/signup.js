35ce4e03f4ec80b28a1d8d45a7dc37a323158786
Cyber28 <cybergaming0628@gmail.com>
2020-07-28 22:19:35 +0200

    fixing stuff > disabling eslint rule

--- src/routers/v1/routes/signup.js
+++ src/routers/v1/routes/signup.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-useless-escape*/
 // o yea, ratelimiting this endpoint sounds like a great idea ngl
 const argon2 = require('argon2');
 const b64 = require('actual-urlsafe-base64');
@@ -6,9 +5,9 @@ const b64 = require('actual-urlsafe-base64');
 async function run(req, res) {
 	if (!req.body.email || !req.body.pass) { return res.status(400).json({ success: false }); }
 
-	// should verify email and password validity with some sick regex, but we good till this doesnt run in production
 	const email = b64.decode(req.body.email).toLowerCase();
-	if (!/[\w.!#$%&'*+-/=?^`{|}~]{1,64}@[a-z0-9\-]{1,255}.[a-z\-]{1,64}/.test(email)) {
+	// if this regex turns out to be broken imma be mad
+	if (!/[\w.!#$%&'*+-/=?^`{|}~]{1,64}@[a-z0-9-]{1,255}.[a-z-]{1,64}/.test(email)) {
 		return res.status(400).json({
 			message: 'invalid email address'
 		});
