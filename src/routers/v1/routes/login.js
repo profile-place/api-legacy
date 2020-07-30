33e2e55d6815097c26e01b61c54621be9ddd85e4
Cyber28 <cybergaming0628@gmail.com>
2020-07-30 14:38:57 +0200

    store token in redis

--- src/routers/v1/routes/login.js
+++ src/routers/v1/routes/login.js
@@ -8,9 +8,8 @@ async function run(req, res) {
 	const email = decode(req.body.email).toLowerCase();
 
 	const user = await req.app.locals.db.collection('user').findOne({ email: email });
-	const match = await argon2.verify(user.password, decode(req.body.pass));
-
 	if (!user) { return res.status(400).json({ message: 'email address doesn\'t exist' }); }
+	const match = await argon2.verify(user.password, decode(req.body.pass));
 	if (!match) { return res.status(401).send(); }
 
 	const token = {
@@ -19,11 +18,12 @@ async function run(req, res) {
 		type: 0,
 		timestamp: new Date().getTime() - constants.epoch
 	};
+	const encodedToken = `${encode(token.owner)}.${encode(token.timestamp.toString())}.${encode(token._id)}`;
 
-	// not saving it to db yet, refer to #api
+	req.app.locals.redis.set(encodedToken, token.type);
 
 	return res.status(200).json({
-		token: `${encode(token.owner)}.${encode(token.timestamp.toString())}.${encode(token._id)}`,
+		token: encodedToken,
 		type: 'Bearer'
 	});
 }
