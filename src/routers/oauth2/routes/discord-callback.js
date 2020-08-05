116ad37a8f0161e2156fd1b7f97e3fdc5bff6360
Cyber28 <cybergaming0628@gmail.com>
2020-08-05 20:18:44 +0200

    improved callbacks

--- src/routers/oauth2/routes/discord-callback.js
+++ src/routers/oauth2/routes/discord-callback.js
@@ -1,7 +1,11 @@
 const centra = require('@aero/centra');
+const { getTokenOwner } = require('../../../../lib/util');
 
 module.exports = {
 	run: async (req, res) => {
+		if (!req.cookies.access_token) { return res.status(400).send(); }
+		console.log(req.app.locals.redis.get(req.cookies.access_token));
+		// should check if the token is valid in the first place
 		const tokens = await centra('https://discord.com/api/oauth2/token', 'POST')
 			.body({
 				client_id: process.env.DISCORD_ID,
@@ -13,11 +17,26 @@ module.exports = {
 			}, 'form')
 			.json();
 
-		const user = await centra('https://discord.com/api/users/@me')
+		let user = await centra('https://discord.com/api/users/@me')
 			.header('Authorization', `Bearer ${tokens.access_token}`)
 			.json();
 
-		res.status(200).json(user);
+		if (user.message === '401: Unauthorized') { return res.status(401).send(); }
+
+		user = Object.assign(user, {
+			_owner: getTokenOwner(req.cookies.access_token),
+			_id: `discord:${user.id}`
+		});
+
+		// if anyone can improve this query, contact Cyber
+		const found = await req.app.locals.db.collection('connection').findOne({ _id: user._id });
+		if (!found) {
+			req.app.locals.db.collection('connection').insertOne(user);
+		} else {
+			req.app.locals.db.collection('connection').findOneAndUpdate({ _id: user._id }, { $set: user });
+		}
+
+		return res.status(200).json(user);
 	},
 	method: 'get'
 };
--- src/routers/oauth2/routes/spotify-callback.js
+++ src/routers/oauth2/routes/spotify-callback.js
@@ -1,4 +1,5 @@
 const centra = require('@aero/centra');
+const { getTokenOwner } = require('../../../../lib/util');
 
 module.exports = {
 	run: async (req, res) => {
@@ -11,11 +12,26 @@ module.exports = {
 			.header('Authorization', `Basic ${Buffer.from(`${process.env.SPOTIFY_ID}:${process.env.SPOTIFY_SECRET}`).toString('base64')}`)
 			.json();
 
-		const user = await centra('https://api.spotify.com/v1/me')
+		let user = await centra('https://api.spotify.com/v1/me')
 			.header('Authorization', `Bearer ${tokens.access_token}`)
 			.json();
 
-		res.status(200).json(user);
+		if (user.error) { return res.status(401).send(); }
+
+		user = Object.assign(user, {
+			_owner: getTokenOwner(req.cookies.access_token),
+			_id: `spotify:${user.id}`
+		});
+
+		// if anyone can improve this query, contact Cyber
+		const found = await req.app.locals.db.collection('connection').findOne({ _id: user._id });
+		if (!found) {
+			req.app.locals.db.collection('connection').insertOne(user);
+		} else {
+			req.app.locals.db.collection('connection').findOneAndUpdate({ _id: user._id }, { $set: user });
+		}
+
+		return res.status(200).json(user);
 	},
 	method: 'get'
 };
